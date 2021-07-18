f87c00e9faa8884373e9f97ea8dc60d25ec099ad
javerianadeem <javerianadeem03@gmail.com>
2021-07-18 12:22:53 +0500

    admin side data retrival of student with fee status

--- app/screens/AdminGetFeeScreen.js
+++ app/screens/AdminGetFeeScreen.js
@@ -18,63 +18,86 @@ import { registrationNumberValidator } from '../core/utils';
 import { Error } from '../components/Error';
 
 const AdminGetFeeScreen = () => {
-    const [registrationNumber, setRegistrationNumber] = useState({ value: '', error: '' });
+    const [registrationNumber, setRegistrationNumber] = useState({ value: '4001096', error: '' });
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState('');
-
+  const [responseError, setResponseError] = useState('');
+  const [studentInfo, setStudentInfo] = useState([]);
+  const [feeStatus, setFeeStatus] = useState([]);
 
   const onSearch = async () => {
+         console.log('reg number of get student', registrationNumber)
     setError('')
-    // setSearchResult([])
+    setStudentInfo([])
+    setFeeStatus([])
+    setResponseError('')
             const registrationNumberError = registrationNumberValidator(registrationNumber.value)
 
         if (registrationNumberError) {
             setRegistrationNumber({ ...registrationNumber, error: registrationNumberError })
+          
             return;
     }
+
+    // 1. user not registered
+    // 2. user registered but no fee status
+    // 3. user registered and has fee status
     try {
       setLoading(true);
-      const request = await axios.post(`${BASE_URL}/route/by_name`, {
-        route_location: locationName.value
-      }).then((response) => {
-       
-        let searchResult = []
-        let search = response.data
-        console.log('response',search[1].route_id)
-        if (search.length > 0) {
-          for (let i = 0; i < search.length; i++) {
-            console.log('route ids',search[i].route_id)
-               if (i == 3 || i == 6 || i == 9 || i == 12 || i == 15) {
-                
-        searchResult.push(
-          <Text key={search[i].route_id} style={{ flexDirection: 'row', justifyContent: 'center', margin: 50 }}>{'\n'}{'\n'} </Text>
-        )
-      }
-      searchResult.push(
-        <OptionItem
-          key={i + 1}
-          bgColor={['#46aeff', '#5884ff']}
-          text={search[i].route_id}
-          onPress={() => {
-            navigation.navigate("ShuttleRouteScreen", {
-              route_id: search[i].route_id 
-            })
-            // this.displayRoutes();
-          }}
+      console.log('reg number of get student', registrationNumber.value)
+      const request = await axios.post(`${BASE_URL}/student/get/`+ registrationNumber.value)
+      .then((response) => {
+        setLoading(false)
+          if(response.data.id === null){
+            // console.log(response.data)
+            setResponseError('The student has not not registered on the application yet')
+            console.log('User is not registered')
+          }
 
-        />
-      )
+          else if(response.data.id.fee_history.length == 0){
+            console.log('No fee history available')
+             let student_info= []
+             student_info.push(
+               <View key={1}>
+               <Text>Name: {response.data.id.st_name}</Text>
+               <Text>Registration No. : {response.data.id.st_reg_number}</Text>
+               <Text>Fee History</Text>
+              </View>
+             )
+             setStudentInfo([student_info])
+            setResponseError('No fee history available for this student')
           }
-          setSearchResult([...searchResult])
-          setLoading(false);
-          // console.log('res', searchResult)
-        }
-        else {
-          if (response.data.length == 0) {
-            setLoading(false);
-            setError("Oops! No shuttle found for your route. Try another location")
+          else {
+        let feeMonthsLength = response.data.id.fee_history;
+        console.log(feeMonthsLength[0])
+        
+          console.log('res', response.data)
+       let student_info= []
+       let fee_status = []
+       student_info.push(
+         <View key={1}>
+         <Text>Name: {response.data.id.st_name}</Text>
+         <Text>Registration No. : {response.data.id.st_reg_number}</Text>
+         <Text>Fee History</Text>
+        </View>
+       )
+       setStudentInfo([student_info])
+       for(let i = 0; i<feeMonthsLength.length; i++){
+        fee_status.push(
+            <View key={i+1}>
+              <Text>Month: {feeMonthsLength[i].paid_month}</Text>
+             <Text>Fee Status: {feeMonthsLength[i].fee_status}</Text>
+            <Text>Paid Date: {feeMonthsLength[i].paid_date}</Text>
+         
+          
+           </View>
+         )
+       }
+
+         
+
+       setFeeStatus([fee_status])
           }
-        }
       })
     } catch (e) {
       console.log(e)
@@ -86,13 +109,13 @@ const AdminGetFeeScreen = () => {
   return (
     
 
-       <SafeAreaView style={{ flex: 1 }}>
-         <View style={styles.container}>
+       <SafeAreaView style={styles.container}>
+         <View >
       <Header title="Check Student Fee Status" />
       </View>
      <BackButton goBack={() => navigation.navigate('StudentHomeScreen')} />
       
-        <ScrollView>
+        {/* <ScrollView> */}
            
           <View style={{ padding: 18, alignItems: 'center' }}>
          
@@ -109,20 +132,34 @@ const AdminGetFeeScreen = () => {
             <View style={styles.row}>
               <Text style={styles.error}>{error}</Text>
             </View>
+            {studentInfo}
+            {feeStatus}
+             <View style={styles.row}>
+              <Text style={styles.error}>{responseError}</Text>
+            </View>
           </View>
 
-          </ScrollView>
+          {/* </ScrollView> */}
+            <Loading loading={loading} />
     </SafeAreaView>
   );
 };
 const styles = StyleSheet.create({
     container: {
-        //  flex: 1,
+         flex: 1,
         // marginTop: 40,
         // marginBottom: 40,
          
     },
- 
+   error: {
+    color: "red"
+  },
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignContent: 'center',
+    // margin: 4,
+  }
   });
 
 export default AdminGetFeeScreen;
